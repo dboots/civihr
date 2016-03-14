896d025a7e9d51f9f924f63fa3e63dfa0e16f405
dboots <don@donboots.com>
2016-03-13 21:37:41 -0400

    gulpfile change

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 
+//-- watch all less files
 gulp.task('default', function() {
 	gulp.watch('css/less/*.less', ['less']);
 });
 
+//-- only process the main LESS file
 gulp.task('less', function() {
    gulp.src('css/less/style.less')
       .pipe(less())
